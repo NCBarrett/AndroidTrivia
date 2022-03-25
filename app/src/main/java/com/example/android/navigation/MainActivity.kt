766f61f7ae06c65e6685ca30733421ce7b76b27d
Neill Barrett <barrettneill@gmail.com>
2022-03-25 12:33:41 -0700

    3/25 - Finished  "Android Kotlin Fundamentals: 03.2 Define navigation paths". Compiles fine.

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,27 +19,34 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.titleFragment) as NavHostFragment
         val navController = navHostFragment.navController
         //val navController = this.findNavController(R.id.titleFragment)
 
         NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.titleFragment)
-        return navController.navigateUp()  // super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)  //navController.navigateUp()  // super.onSupportNavigateUp()
     }
 
 }
