1fffb2592667a91663676fc59dfc631489eea62d
Neill Barrett <barrettneill@gmail.com>
2022-03-27 13:29:54 -0700

    3/25 - Finished  "Android Kotlin Fundamentals: 03.2 Define navigation paths". Compiles fine.

--- app/src/main/java/com/example/android/navigation/RulesFragment.kt
+++ app/src/main/java/com/example/android/navigation/RulesFragment.kt
@@ -19,13 +19,19 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class RulesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_rules, container, false)
     }
+
+
 }
